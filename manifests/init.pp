7b75cb168aef097736efb9b47d3c46cd80695af2
natewalck <nate@osxadm.in>
2013-09-16 06:59:56 -0400

    add initial variables to init for the windows_package_handler defined type

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,12 @@
-class windows_package_handler (
+define windows_package_handler (
+  $ensure            => 'present',
+  $source,           => undef
+  $package_name,     => undef
+  $install_options   => undef,
+  $installable       => undef,
+  $uninstall_options => undef,
+  $uninstallable     => undef,
   ){
   
-  class{'windows_package_handler::applications': } ->
-  Class["windows_package_handler"]
 
 }
